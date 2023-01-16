9ad6e15a64f78b098f91bf3e4ce9cc18b3fc21cb
mmatesic <mario.matesic@gmail.com>
2023-01-16 09:23:59 +0100

    implement Metrics class

--- lib/metrics.rb
+++ lib/metrics.rb
@@ -1,3 +1,13 @@
 class Metrics
+    def initialize()
+        @log_metrics = Hash.new
+    end
 
+    def add_metric(address, ip)
+
+    end
+
+    def get_metrics
+        @log_metrics
+    end
 end
