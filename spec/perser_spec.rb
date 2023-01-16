de89157635b376f38d2b8a3db728772de9d813f7
mmatesic <mario.matesic@gmail.com>
2023-01-16 08:52:17 +0100

    test adding ip addresses to metric item

--- spec/perser_spec.rb
+++ spec/perser_spec.rb
@@ -8,4 +8,25 @@ describe MetricItem do
         expect(metric_item.web_address).to eql "/web_adress"
         expect(metric_item.ip_addresses).to eql Set["255.255.255.255"]
     end
+
+    context "MetricItem class should add ip addresses" do
+
+        it "should implement add_ip" do
+            expect(item_instance).to respond_to(:add_ip)
+        end
+
+        it "should add ip address" do
+            metric_item = item_instance
+            metric_item.add_ip("255.255.255.254")
+            expect(metric_item.ip_addresses.size).to eq 2
+            expect(metric_item.ip_addresses).to eq Set["255.255.255.255", "255.255.255.254"]
+        end
+
+        it "should not add ip address if already exists" do
+            metric_item = item_instance
+            metric_item.add_ip("255.255.255.255")
+            expect(metric_item.ip_addresses.size).to eq 1
+            expect(metric_item.ip_addresses).to eq Set["255.255.255.255"]
+        end
+    end
 end
