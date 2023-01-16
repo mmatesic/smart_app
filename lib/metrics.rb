46345fbff5eee9148799b516bac8c1b65ea13c74
mmatesic <mario.matesic@gmail.com>
2023-01-16 10:03:59 +0100

    add metrics logic

--- lib/metrics.rb
+++ lib/metrics.rb
@@ -4,7 +4,14 @@ class Metrics
     end
 
     def add_metric(address, ip)
-
+        if @log_metrics[address].nil?
+            metric_item = MetricItem.new(web_address: address, ip_address: ip)
+            @log_metrics[address] = metric_item
+        else
+            metric_item = @log_metrics[address]
+            metric_item.add_ip(ip)
+            @log_metrics[address] = metric_item
+        end
     end
 
     def get_metrics
