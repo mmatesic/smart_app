3b81534406c4a905d9dfe85e59820efb31110814
mmatesic <mario.matesic@gmail.com>
2023-01-16 15:26:41 +0100

    add usage example

--- parser.rb
+++ parser.rb
@@ -2,8 +2,14 @@
 
 require './lib/parser.rb'
 
+unless ARGV[0]
+    puts "Usage: ruby parser.rb <filename.log>"
+    puts "Example: ruby parser.rb webserver.log>"
+    exit
+end
+
 parser = Parser.new
-parser.read_file(ARGV[0])
+parser.read_file(ARGV[0].chomp)
 parser.report_visits_count
 puts "---------------------------------------------------"
 parser.report_unique_visits_count
