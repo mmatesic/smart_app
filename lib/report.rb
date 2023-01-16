ef7f06a66824c123d5e286e05d233843688cb1bc
mmatesic <mario.matesic@gmail.com>
2023-01-16 12:52:21 +0100

    sort report results

--- lib/report.rb
+++ lib/report.rb
@@ -1,10 +1,15 @@
 class Report<Metrics
     def get_visits
-        super()
+        sort_metrics(super())
     end
 
     def get_unique_visits
-        super()
+        sort_metrics(super())
     end
 
+    private
+
+    def sort_metrics(metrics)
+        metrics.sort_by{|k, v| [-v, k]}.to_h
+    end
 end
