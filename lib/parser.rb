dbb0ac6ba3e3a44714c16163a717360c389780f3
mmatesic <mario.matesic@gmail.com>
2023-01-16 14:28:51 +0100

    initalize Parser class

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,2 +1,13 @@
 class Parser
+    def initialize
+        @report = Report.new
+    end
+
+    def report_visits_count
+        @report.get_metrics
+    end
+
+    def report_unique_visits_count
+        @report.get_unique_visits
+    end
 end
