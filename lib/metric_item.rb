9e861de56cca46111dfdae94ffc4480fbd691d27
mmatesic <mario.matesic@gmail.com>
2023-01-16 09:09:39 +0100

    add visit counting to metric item

--- lib/metric_item.rb
+++ lib/metric_item.rb
@@ -1,13 +1,15 @@
 require 'set'
 
 class MetricItem
-    attr_reader :web_address, :ip_addresses
+    attr_reader :web_address, :ip_addresses, :visit_count
     def initialize(web_address:, ip_address:)
         @web_address = web_address
+        @visit_count = 1
         @ip_addresses = Set.new([ip_address])
     end
 
     def add_ip(ip)
+        @visit_count += 1
         @ip_addresses << ip
     end
 end
